9e04600f50556c7170e317b0c67fe6e4dcb8e1a9
Michie <contact@michiewillman.com>
2023-06-07 22:51:07 -0700

    Update data call syntax

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,86 +1,66 @@
 // Global variables
+var currentDate = dayjs().format('DD/MM/YYYY');
 var searchContainer = $('#search-form');
+var cityInput = $('#city-input');
 var todaysWeather = $('#weather-container');
 var fiveDay = $('five-day');
 
 
-function getApi() {
-  // Declare variable for the weather api URL using personal key
-  var requestURL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={6de9315fe02ad136b310b6c68d6d0811}?per_page=6";
-
-
+function getWeather(city) {
   // Make a call to the api for weather information
+  var requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=6de9315fe02ad136b310b6c68d6d0811&units=imperial";
+
   fetch (requestURL)
   .then(function (response) {
-    return response.json();
-  })
-  .then(function(data) {
-
-    for (var i = 0; i < data.length; i++) {
-      // Add icons 
-      var iconDiv = $('.card-icon');
-      iconDiv.val(data[i]);
-
-      if (i === 0) {
-        // Display current day weather in weatherContainer
-        var mainCityDiv = $('#main-city');
-        var currentDateDiv = $('#current-date');
-        var currentDate = dayjs().format('DD/MM/YYYY');
-        var cityTempDiv = $('.city-temp');
-        var cityWindDiv = $('.city-wind');
-        var cityHumidDiv = $('.city-humidity');
-
-        mainCityDiv.val(data[i].city.name);
-        cityTempDiv.va();
-        cityWindDiv.va();
-        cityHumidDiv.val();
-
-        console.log(currentDate);
-        currentDateDiv.val(currentDate);
-
-      } else if (i > 0) {
-        // Create the elements for data to go in on each card
-        var cardDate = $('.card-date');s
-        var cardTemp = $('.card-temp');
-        var cardWind = $('.card-temp');
-        var cardHumidity = $('.card-temp');
-
-        // Add values to divs
-        cardDate.textContent = data[i].list.dt;
-        cardIcon.textContent = data[i].list.weather.icon;
-        cardTemp.val(data[i].list.main.temp);
-        cardWind.val(data[i].list.wind.speed);
-        cardHumidity.val(data[i].list.main.humidity);
-
-
-      }
-
-      // TODO: figure out how to store this info to use later... in an object array?
-
-      return;
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data);
+        displayWeather(data);
+      });
+    } else {
+      alert('Error: ' + response.statusText);
     }
-
-  })
+  });
 }
 
+function displayWeather(data) {
+  for (var i = 0; i < data.length; i++) {
 
-// Autocomplete widget for city name
-$(function () {
-  var availCities = [];
-  $('#city-input').autocomplete({
-    source: availCities,
-  });
-});
+    console.log(data[i].dt);
+
+    // Display current day weather in weatherContainer
+    $('#main-city').textContent = toString(city);
+    $('#current-date').textContent = currentDate;
+    $('.city-temp').textContent = "Temp:" + data[0].main.temp + "°F";
+    $('.city-wind').textContent = "Wind" + data[0].wind.speed;
+    $('.city-humidity').textContent = "Humidity" + data[0].main.humidity;
+
+    // Add icons 
+    var iconDiv = $('.card-icon');
+    iconDiv.textContent = data[i].weather.icon;
+
+    if (i > 0) {
+      // Add values to 5 day weather cards
+      $('.card-date').textContent = data[i].dt;
+      $('.card-temp').textContent = "Temp:" + data[i].main.temp;
+      ('.card-wind').textContent = "Wind" + data[i].wind.speed;
+      $('.card-humidity').textContent = "Humidity" + data[i].main.humidity;
+    }
+    return;
+  }
+}
 
 function searchHandler(event) {
   event.preventDefault();
 
-  var city = $('#city-input').val();
+  var selectCity = $('#city-input').val();
 
-  if (city) {
-    getApi();
+  if (selectCity) {
+    getWeather(selectCity);
+
+    // TODO: clear the input form
   } else {
-    return;
+    alert('Please enter a valid city name.')
   }
 
   // TODO: Save city search to local storage
@@ -88,16 +68,22 @@ function searchHandler(event) {
 
 }
 
-function renderLastCity() {
+// function renderLastCity() {
+//   var saved = localStorage.getItem('savedCity');
+//   if (saved) {
 
-}
+//   } else {
+//     return;
+//   }
+
+// }
 
 // Event Listener on search button
-var searchBtn = $('#search-button');
+var searchBtn = $('.search-button');
 searchBtn.on("click", searchHandler);
 
-// TODO: Render last saved city search on page load
-renderLastCity();
+// Render last saved city search on page load
+// renderLastCity();
 
 
 // HOW TO MAKE THIS APP BETTER IN THE FUTURE:
